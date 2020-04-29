893e1c55d9da3f87ddacde0781fdedbb22be3a90
Sylwia Olejko <sylwia.olejko@gmail.com>
2020-04-29 20:07:26 +0200

    5.3

---
+++ js/script.js
@@ -0,0 +1,6 @@
+printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = 'kamień';
+printMessage ('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let palyerMove = 'papier';
+printMessage ( 'Zagrałem' + palyerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
